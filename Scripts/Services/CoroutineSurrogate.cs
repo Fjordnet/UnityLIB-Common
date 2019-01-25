a387c78e919a91821b60d5332527e0cc1a1b0257
Theo Christensen <t.e.christensen@accenture.com>
2019-01-25 13:52:14 -0600

    Adding CoroutineSurrogate, which provides coroutine access for non-MonoBehaviours.

---
+++ Scripts/Services/CoroutineSurrogate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fjord.Common.Services
+{
+    /// <summary>
+    /// Provides access to Coroutines for non-MonoBehaviour objects.
+    /// </summary>
+    public class CoroutineSurrogate : MonoBehaviour
+    {
+        public IEnumerator Iterator;
+
+        public static void Instantiate(IEnumerator co)
+        {
+            GameObject surrogate = new GameObject("CoroutineSurrogate");
+            CoroutineSurrogate script = surrogate.AddComponent<CoroutineSurrogate>();
+            script.Iterator = co;
+            script.RunCoroutine();
+        }
+
+        public void RunCoroutine()
+        {
+            StartCoroutine(RunCoroutineCo());
+        }
+
+        IEnumerator RunCoroutineCo()
+        {
+            yield return StartCoroutine(Iterator);
+            Destroy(gameObject);
+        }
+    }
+}
