4752fc3fb33e19517adc3ca75be1e497aea49f8e
Theo Christensen <t.e.christensen@accenture.com>
2019-08-15 11:52:40 -0500

    Adding the BroadcastSystem (renamed to EventDispatcher).

---
+++ Scripts/Services/AnimationBroadcaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Fjord.Common.BroadcastSystem
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimationBroadcaster : MonoBehaviour
+    {
+        private void BroadcastAnimationEvent(string eventName)
+        {
+            BroadcastSystem.instance.Broadcast(eventName, null);
+        }
+    }
+}
