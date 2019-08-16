2a7b83304170389f87838ee77ad4350b8de3db77
Theo Christensen <t.e.christensen@accenture.com>
2019-08-16 11:41:44 -0500

    Moving EventDispatcher and associated scripts to Fjord.Common.Events namespace/folder. EventDispatcher now inherits from SingletonBehavour. SingletonBehavour now instantiates a new GameObject and component if one does not exist in the scene.

---
+++ Scripts/Events/EventDispatcher.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Fjord.Common.Components;
+
+namespace Fjord.Common.Events
+{
+    public class EventDispatcher : SingletonBehaviour<EventDispatcher>
+    {
+        /// <summary>
+        /// Returns true if an instance exists.
+        /// </summary>
+        public static bool IsInstantiated { get { return _instance != null; } }
+
+        public bool DoLogging = true;
+
+        [SerializeField]
+        private Dictionary<string, StandardEvent> eventMap = new Dictionary<string, StandardEvent>();
+
+        public void RegisterListener(string eventName, UnityAction<object, object> callback)
+        {
+            if (!eventMap.ContainsKey(eventName))
+            {
+                eventMap.Add(eventName, new StandardEvent());
+            }
+
+            eventMap[eventName].AddListener(callback);
+        }
+        public void UnregisterListener(string eventName, UnityAction<object, object> callback)
+        {
+            if (eventMap.ContainsKey(eventName))
+            {
+                eventMap[eventName].RemoveListener(callback);
+                if(eventMap[eventName].GetPersistentEventCount() == 0)
+                {
+                    eventMap.Remove(eventName);
+                }
+            }
+        }
+        public void Dispatch(string eventName, object sender, object eventArg)
+        {
+            if (DoLogging)
+            {
+                Debug.Log("Dispatching event: " + eventName + " " + eventArg.ToString() + " for sender: " + sender.ToString());
+            }
+
+            if (eventMap.ContainsKey(eventName))
+            {
+                eventMap[eventName].Invoke(sender, eventArg);
+            }
+        }
+    }
+}
---
+++ Scripts/Events/EventSender.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Fjord.Common.Events
+{
+    public class TriggerDispatchEvent : MonoBehaviour
+    {
+        private void TriggerDispatch(string eventName, object eventArg)
+        {
+            EventDispatcher.Instance.Dispatch(eventName, this, null);
+        }
+    }
+}
