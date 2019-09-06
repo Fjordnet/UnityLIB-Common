2e28b4ce85e0bafc1690d5e7ba198f5faa22eaef
Theo Christensen <t.e.christensen@accenture.com>
2019-09-06 11:17:20 -0500

    Adding PersistentServices script which creates a singleton prefab immediately upon scene load. Child service objects can be added to the prefab for global access.

---
+++ Scripts/Services/PersistentServices.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fjord.Common.Services
+{
+    public class PersistentServices : MonoBehaviour
+    {
+        [SerializeField]
+        private bool _instantiateBeforeSceneLoad = true;
+
+        static private PersistentServices _instance;
+
+        static public PersistentServices Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    _instance = FindObjectOfType<PersistentServices>();
+                }
+                return _instance;
+            }
+        }
+
+        public bool InstantiateBeforeSceneLoad { get { return _instantiateBeforeSceneLoad; } }
+
+        static public T GetService<T>()
+        {
+            return Instance.GetComponentInChildren<T>();
+        }
+
+        static public T[] GetServices<T>()
+        {
+            return Instance.GetComponentsInChildren<T>();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static private void Init()
+        {
+            if (null == _instance)
+            {
+                PersistentServices prefab = Resources.Load<PersistentServices>("PersistentServices");
+                if (null != prefab && prefab.InstantiateBeforeSceneLoad)
+                {
+                    DontDestroyOnLoad(Instantiate(prefab));
+                    Debug.Log("Instantiated Persistent Services.");
+                }
+                else
+                {
+                    Debug.LogWarning("Persistent Services already instantiated.");
+                }
+            }
+        }
+    }
+}
